c51ee0271d5596c919ba2d618b385f0bdce141b8
Hai Tran <mbp-kubuntu@bcraft.id.vn>
2026-07-09 11:14:10 +0700

    feat: add rough models

--- src/QuanLySanBong/DoAn1_Nhom2_DHTI17A3HN.java
+++ src/QuanLySanBong/DoAn1_Nhom2_DHTI17A3HN.java
@@ -14,7 +14,7 @@ public class DoAn1_Nhom2_DHTI17A3HN {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Interface.DangNhap.main(args);
     }
     
 }
